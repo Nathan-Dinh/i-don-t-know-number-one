1803767166292d9bc4d8ca805d5e4f852fd61c03
Nathan Dinh <nathan.dinh03@outlook.com>
2025-01-11 04:02:30 -0500

    Feature: Initial layout of api call to user info

---
+++ src/apis/userInfo.tsx
@@ -0,0 +1,10 @@
+const fetchUserInfo = async () => {
+  const res = await fetch('http://localhost:3000/v1/session/validate', {
+    method: 'GET',
+    credentials: 'include',
+  });
+  if (!res.ok) throw new Error('Session is not good');
+  return await res.json();
+};
+
+export { fetchValidSession };
