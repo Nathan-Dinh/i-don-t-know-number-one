a380dbd3c23cb2861cc22b31857440f13acb57f8
Nathan Dinh <nathan.dinh03@outlook.com>
2025-01-11 04:01:02 -0500

    Feature: Added wrapper to app tag

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,19 @@
+/* eslint-disable no-undef */
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './main.css';
+import { AuthContextProvider } from './contexts/AuthContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </QueryClientProvider>
+  </StrictMode>,
 );
